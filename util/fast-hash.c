5d73065d7316b96d5992901cbf59b078943a64f0
izzy2lost <izzynochill@gmail.com>
2026-03-20 14:36:11 -0400

    real XXH3 hash

--- util/fast-hash.c
+++ util/fast-hash.c
@@ -1,7 +1,33 @@
 #include "qemu/fast-hash.h"
 #include <xxhash.h>
 
+#ifdef __ANDROID__
+static uint64_t fast_hash_android_tiny(const uint8_t *data, size_t len)
+{
+    const uint64_t fnv_offset = 1469598103934665603ULL;
+    const uint64_t fnv_prime = 1099511628211ULL;
+    uint64_t hash = fnv_offset;
+
+    for (size_t i = 0; i < len; i++) {
+        hash ^= data[i];
+        hash *= fnv_prime;
+    }
+
+    return hash;
+}
+#endif
+
 uint64_t fast_hash(const uint8_t *data, size_t len)
 {
+#ifdef __ANDROID__
+    /*
+     * XXH3 is a clear win for the larger shader-state keys Android hashes,
+     * but very small TCG blocks can spend more time in XXH3 setup than in the
+     * hash itself. Keep a tiny-input fast path for those cases.
+     */
+    if (len <= 4) {
+        return fast_hash_android_tiny(data, len);
+    }
+#endif
     return XXH3_64bits(data, len);
 }
