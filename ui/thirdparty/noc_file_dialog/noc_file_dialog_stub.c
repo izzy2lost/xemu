86dfb6ecfadcbac9d6bffc43d40765af477c155c
alejomazabuel <alejandro.mazabuel@gmail.com>
2026-03-24 10:07:39 -0500

    iOS: stub noc file dialog in embedded core

---
+++ ui/thirdparty/noc_file_dialog/noc_file_dialog_stub.c
@@ -0,0 +1,13 @@
+#include "noc_file_dialog.h"
+
+const char *noc_file_dialog_open(int flags,
+                                 const char *filters,
+                                 const char *default_path,
+                                 const char *default_name)
+{
+    (void)flags;
+    (void)filters;
+    (void)default_path;
+    (void)default_name;
+    return NULL;
+}
